9ac3a92b5d464c5ead5385a6bda862dbf60c111e
Pranjay Poddar <52539501+pranjay-poddar@users.noreply.github.com>
2021-04-21 16:02:17 +0530

    Create leap_year.py

---
+++ algorithms/leap year check/leap_year.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+    leap = False    
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True    
+    return leap_year
