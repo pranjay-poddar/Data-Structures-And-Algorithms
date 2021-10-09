008f79d61575f3cf753570842c0dca8270b0caa7
addy0110 <addycoder0110@gmail.com>
2021-10-09 13:34:35 +0530

    Update nthFibonacciNum.cpp

--- dynamic programming/c++/nthFibonacciNum.cpp
+++ dynamic programming/c++/nthFibonacciNum.cpp
@@ -6,12 +6,12 @@ int main(){
     cout<<"Enter the value of n : ";
     cin>>n;
 
-    vector<ll>dp(n+1,0);
-    dp[2] = 1;
+    vector<ll>dp(n+1,0);            // Initializing dp vector which will store the ith fibnoacci number at ith index
+    dp[2] = 1;                      // Defining base case
 
     for(int i = 3 ; i <= n ; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] = dp[i-1] + dp[i-2];  // Using previous results to obtain the next 
     }
-    cout<<"Finonacci Number : "<<dp[n];
+    cout<<"Finonacci Number : "<<dp[n];     // Printing the nth fibnonacci number
 return 0;
 }
