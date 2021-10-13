0938c610be796ca6a6c4874caf7223a20a743ba9
Rohan Singh Rathore <rohansinghrathore00@gmail.com>
2021-10-13 16:30:44 +0530

    Update Merge-Two_Sorted_Arrays.cpp

--- arrays/c++/Merge-Two_Sorted_Arrays.cpp
+++ arrays/c++/Merge-Two_Sorted_Arrays.cpp
@@ -1,77 +1,51 @@
-void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
-{
-    
-    int i=0,j=0,k=0;
-    
-    while(i<size1 && j<size2)
-    {
-        
-		if(arr1[i]<arr2[j])
-        	{
-				ans[k]=arr1[i];
-                i++;
-            	k++;
-        	}
-        else
-            {
-                ans[k]=arr2[j];
-                j++;
-            	k++;
-            }
-    }
-    
-    while(i<size1)
-    {
-        ans[k]=arr1[i];
-                i++;
-            	k++;
-    }
-    while(j<size2)
-    {
-         ans[k]=arr2[j];
-                j++;
-            	k++;
-	}
-}
-#include <iostream>
+// C++ program to merge two sorted arrays/
+#include<iostream>
 using namespace std;
 
-#include "solution.h"
+// Merge arr1[0..n1-1] and arr2[0..n2-1] into
+// arr3[0..n1+n2-1]
+void mergeArrays(int arr1[], int arr2[], int n1,int n2, int arr3[])
+{
+	int i = 0, j = 0, k = 0;
 
+	// Traverse both array
+	while (i<n1 && j <n2)
+	{
+		// Check if current element of first
+		// array is smaller than current element
+		// of second array. If yes, store first
+		// array element and increment first array
+		// index. Otherwise do same with second array
+		if (arr1[i] < arr2[j])
+			arr3[k++] = arr1[i++];
+		else
+			arr3[k++] = arr2[j++];
+	}
+
+	// Store remaining elements of first array
+	while (i < n1)
+		arr3[k++] = arr1[i++];
+
+	// Store remaining elements of second array
+	while (j < n2)
+		arr3[k++] = arr2[j++];
+}
+
+// Driver code
 int main()
 {
-		int size1;
-		cin >> size1;
+	int arr1[] = {1, 3, 5, 7};
+	int n1 = sizeof(arr1) / sizeof(arr1[0]);
 
-		int *arr1 = new int[size1];
+	int arr2[] = {2, 4, 6, 8};
+	int n2 = sizeof(arr2) / sizeof(arr2[0]);
 
-		for (int i = 0; i < size1; i++)
-		{
-			cin >> arr1[i];
-		}
+	int arr3[n1+n2];
+	mergeArrays(arr1, arr2, n1, n2, arr3);
 
-		int size2;
-		cin >> size2;
+	cout << "Array after merging" <<endl;
+	for (int i=0; i < n1+n2; i++)
+		cout << arr3[i] << " ";
 
-		int *arr2 = new int[size2];
-
-		for (int i = 0; i < size2; i++)
-		{
-			cin >> arr2[i];
-		}
-
-		int *ans = new int[size1 + size2];
-
-		merge(arr1, size1, arr2, size2, ans);
-
-		for (int i = 0; i < size1 + size2; i++)
-		{
-			cout << ans[i] << " ";
-		}
-
-		cout << endl;
-		delete[] arr1;
-		delete[] arr2;
-		delete[] ans;
-	
+	return 0;
 }
