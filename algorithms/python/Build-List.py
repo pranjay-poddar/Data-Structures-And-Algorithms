2ea8b0551d6030f008b0c08751ac0fa4d4c8df87
Suvrat Patel <72069889+MadJokkerr@users.noreply.github.com>
2021-10-13 07:37:06 -0800

    Build-List

---
+++ algorithms/python/Build-List.py
@@ -0,0 +1,28 @@
+#SCRIPT FOR CREATION OF LIST.
+#YOU CAN ADD ANYTHING IN LSIT WHETHER IT IS STRING, INTEGER , DECIMAL VALUE OR ANYTHINGS BUT KEEP RULE IN MIND.
+
+# Python program to demonstrate 
+# Creation of List 
+  
+# Creating a List
+List = []
+print("Blank List: ")
+print(List)
+  
+# Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+  
+# Creating a List of strings and accessing
+# using index
+List = ["item1", "item2", "item3"]
+print("\nList Items: ")
+print(List[0]) 
+print(List[2])
+  
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Have', 'A'] , ['GOOD'],['DAY'] ]
+print("\nMulti-Dimensional List: ")
+print(List)
