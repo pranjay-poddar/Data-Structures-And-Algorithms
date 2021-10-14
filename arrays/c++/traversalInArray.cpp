4a847085b8d25290b6a31b5013c9b603a8c666a5
Riya Jaiswal <riyajaiswal1916@gmail.com>
2021-10-14 10:03:59 +0530

    added Array traversal code

---
+++ arrays/c++/traversalInArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+ int arr[5]={10, 0, 20, 0, 30};      
+        //traversing array    
+        for (int i = 0; i < 5; i++)    
+        {    
+            cout<<arr[i]<<"\n";    
+        }    
+} 
